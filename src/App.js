ebaf6469754882a5a42ddbdad89342540748c109
Tahar Mustapha <mtahar1998@gmail.com>
2023-06-04 17:44:05 -0400

    BLX-XXX : added hls media player

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ReactHlsPlayer from "react-hls-player";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <ReactHlsPlayer
+          src="http://127.0.0.1:8080/stream/playlist.m3u8"
+          autoPlay={false}
+          controls={true}
+          width="100%"
+          height="auto"
+        />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
